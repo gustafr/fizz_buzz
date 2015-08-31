b15e82c62c31631741a28df77d3f020afa8cfb98
Gustaf Rudholm <gustaf.rudholm2gmail.com>
2015-08-31 16:24:47 +0200

    added user input

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,10 +1,20 @@
+def start_number
+  puts 'Ange startnummer'
+  gets.chomp
+
+end
+
+def end_number
+  puts 'Ange slutnummer'
+  gets.chomp
+end
 
 def play_game
-  (1..100).each {|num| puts fizz_buzz(num) }
+  (start_number.to_i..end_number.to_i).each {|num| puts fizz_buzz(num) }
 end
 
 def fizz_buzz(number)
-  if number % 15 == 0 
+  if number % 15 == 0
     return 'FizzBuzz'
   elsif number % 5 == 0
     return 'Buzz'
